fadaf079521eabbb81ce4ae77ca414bdf0b8f41b
Brian O'Keefe <brian@bokstuff.com>
2011-10-29 22:05:49 -0400

    Added intro dialog box, removed bok-noshave

--- bok-noshave/noshave.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /Users/bok/Pictures/Photo\ Booth\ Library/Pictures
-old=$(ls -t | head -1)
-new=$(date +%d).jpg
-mv "$name" $new
-scp $new bok@bokstuff.com:/var/www/stkbuzz.com/public_html/noshave/pics
-ssh bokstuff.com "cd /var/www/stkbuzz.com/public_html/noshave && ./genjson"
